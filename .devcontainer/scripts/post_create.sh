c6c1f7d401c86dce105484834b13096134042d5d
tomo1227 <tomo.soccer1227@gmail.com>
2024-07-15 08:58:03 +0000

    bunの追加

--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/env sh
+# #!/usr/bin/bash
+
+wget -qO- https://bun.sh/install | bash
 
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
 echo "source ~/.git-completion.bash" >> ~/.bashrc
 
 # bash auto completion
-sudo apk update && sudo apk add bash-completion
+sudo apt update && sudo apt install bash-completion
